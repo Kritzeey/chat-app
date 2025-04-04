01142ced8b5c33d428ab798d6cd271b4cc3c2583
valhize <190005533+Kritzeey@users.noreply.github.com>
2025-04-04 16:09:17 +0700

    Made basic signup API

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,9 +1,33 @@
+import User from "../models/user.model.js";
 
 export async function signUp(req, res) {
     try {
         const { name, username, password, confirmPassword } = req.body;
-    } catch (error) {
 
+        if (password != confirmPassword) {
+            return res.status(400).json({ error: "Passwords didn't match" });
+        }
+
+        const user = await User.findOne({ username });
+
+        if (user) {
+            return res.status(400).json({ error: "Username already exists" });
+        }
+
+        const profilePic = "https://avatar.iran.liara.run/public";
+
+        const newUser = new User({
+            name,
+            username,
+            password,
+            profilePicture: profilePic,
+        });
+
+        await newUser.save();
+
+        res.status(201).json(newUser);
+    } catch (error) {
+        res.status(500).json({ error: "Internal server error" })
     }
 }
 
